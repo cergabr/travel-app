8b39141d80aec5755b6303f89aea71e3065f4111
cergabr <gabriele.cerqueglini@gmail.com>
2021-06-24 15:37:32 +0200

    add getImage route

--- src/server/server.js
+++ src/server/server.js
@@ -16,7 +16,22 @@ app.use(express.json());
 
 /* cors */
 const cors = require('cors');
+const { request } = require("http");
 app.use(cors());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    next();
+});
+
+app.get("/getImage", (request,response)=>{
+    fetch("https://pixabay.com/api/?key="+process.env.PIXABAY_KEY+"&q="+encodeURI(request.query.city)+"&image_type=photo&category=travel")
+    .then(res=>{return res.json()})
+    .then(data=>{
+        console.log(data.hits[0])
+        return response.json({imgURL: data.hits[0].largeImageURL})
+    })
+});
 
 /* Initialize the main project folder */
 app.use(express.static('dist'));
@@ -29,21 +44,11 @@ const port = 8080;
 app.listen(port, ()=>{console.log(`server running on localhost: ${port}`)});
 
 app.get('/apiKeys',(req,res)=>{
-    res.send({
+    projectData.apiKeys = {
         geonm: process.env.GEONAMES_KEY,
         wbit: process.env.WEATHERBIT_KEY,
         pxby: process.env.PIXABAY_KEY
-    });
-});
-
-app.get('/all', (req,res)=>{res.send(projectData)});
-
-app.post('/addGeo',(req, res)=>{
-    const geodata = {
-        lat: req.body.lat,
-        lon: req.body.lon,
-    }
-    projectData.geodata=geodata;
-    res.send(projectData);
-    console.log(projectData);
+    };
+    console.log(projectData.apiKeys)
+    res.send(projectData.apiKeys);
 });
