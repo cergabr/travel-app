2b87185d6383b3ee70ff0d806dd79da5e1d8c2b4
cergabr <gabriele.cerqueglini@gmail.com>
2021-06-25 20:28:36 +0200

    add req.context

--- src/client/app.js
+++ src/client/app.js
@@ -2,8 +2,20 @@ import {handleSubmit} from "./js/formHandler";
 import {getTravelData} from "./js/getTravelData";
 import {inputValidate} from "./js/inputValidate";
 
-import "./styles/base.scss";
+function importAll(r) {
+  r.keys().forEach(r);
+}
+importAll(require.context("./img/icons", false, /\.png$/));
+
 import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
+
+import "./img/bg.jpg";
+import "./img/logo_pixabay.png";
+ 
 
 export{
   handleSubmit,
